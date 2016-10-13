456fd50d129b0d77aec5007159b4252f3126f149
olivine13 <765495982@qq.com>
2016-10-13 19:25:03 +0800

    no message

---
+++ app/src/main/java/org/lowcarbon/soda/model/RoadInfo.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2016 CVTE All Rights Reserved.
+ */
+package org.lowcarbon.soda.model;
+
+/**
+ * @Description: TODO
+ * @author: laizhenqi
+ * @date: 2016/10/13
+ */
+public class RoadInfo {
+}
